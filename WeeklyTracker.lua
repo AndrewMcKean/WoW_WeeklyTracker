ba45e8566d09d53bc5469d3fffd32c5b8b817b71
Andrew McKean <rook0r3@live.co.uk>
2025-04-10 21:33:28 +0100

    Small wording changes

--- WeeklyTracker.lua
+++ WeeklyTracker.lua
@@ -5,44 +5,44 @@ local function OnEnteringWorld(self, event, isLogin)
     local playerName = UnitName("player")
 
     -- Get keys
-    local chestsOpened=0
+    local keysRecieved=0
 
     for i,q in ipairs({84736,84737,84738,84739})
       do
-        chestsOpened=chestsOpened+(C_QuestLog.IsQuestFlaggedCompleted(q) and 1 or 0)
+        keysRecieved=keysRecieved+(C_QuestLog.IsQuestFlaggedCompleted(q) and 1 or 0)
       end
 
     -- If there is an existing chestVal and it's less than chestsOpened it must be a new week so wipe DB
     local existingChestVal = WeeklyTrackerDB[""..playerName.."_chestsOpened"] ~= nil
 
     if existingChestVal then
-      if WeeklyTrackerDB[""..playerName.."_chestsOpened"] < chestsOpened then
+      if WeeklyTrackerDB[""..playerName.."_chestsOpened"] < keysRecieved then
         print("Welcome to a new week! DB Wiped")
         WeeklyTrackerDB = {}
       end
     end
 
     --  Set keys
-    WeeklyTrackerDB[""..playerName.."_chestsOpened"] = chestsOpened
+    WeeklyTrackerDB[""..playerName.."keys"] = keysRecieved
 
     -- Get and set bounty status
     local delversBountyComplete = C_QuestLog.IsQuestFlaggedCompleted(86371)
 
     WeeklyTrackerDB[""..playerName.."_bounty"] = delversBountyComplete
 
-    chestsOpened = WeeklyTrackerDB[""..playerName.."_chestsOpened"]
+    keysRecieved = WeeklyTrackerDB[""..playerName.."keys"]
     local bountyStatus = WeeklyTrackerDB[""..playerName.."_bounty"]
 
     local delversBountyMessage = " has NOT completed delvers bounty"
 
     if bountyStatus == true then
-      delversBountyMessage = "HAS completed delvers bounty"
+      delversBountyMessage = " HAS completed delvers bounty"
     end
 
     -- Log to user
     print(playerName .. delversBountyMessage)
 
-    print(playerName .. " has opened ", chestsOpened,'chests')
+    print(playerName .. " has obtained", keysRecieved,'keys')
   end
 end
 
